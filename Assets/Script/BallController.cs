d838a52225903c4cb24c74a50eb65c84c245c142
Paula <paula.banachh@gmail.com>
2023-05-22 14:47:09 +0200

    ok, już jest coraz lepiej XDD

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private GameObject bialaBila = null;
-    [SerializeField] private float power = 0.1f;
-    [SerializeField] private Transform arrow = null;
-    [SerializeField] private List<ColorBall>ballList = new List<ColorBall>();
 
-    private Vector2 mousePosition = Vector2.zero;
-    private Rigidbody2D bialaRigid = null;
-    private Vector2 bialaBilaDefaultPosition = Vector2.zero;
+    public Material material;
+    public float startWidth = 0.2f;
+    public float endWidth = 0.0f;
+    public Color startColour = Color.white;
+    public Color endColour = Color.clear;
+
+    private LineRenderer lineRenderer;
+
+    [SerializeField] GameObject bialaBila = null;
+    [SerializeField] float power = 20f;
+    [SerializeField] Transform arrow = null;
+    [SerializeField] List<ColorBall>ballList = new List<ColorBall>();
+
+    Vector2 mousePosition = new Vector3();
+    Rigidbody2D bialaRigid = null;
+    Vector2 bialaBilaDefaultPosition = new Vector2();
 
     // Start is called before the first frame update
     void Start()
     {
         bialaRigid = bialaBila.GetComponent<Rigidbody2D>();
-        bialaBilaDefaultPosition = bialaBila.transform.position;
+        bialaBilaDefaultPosition = bialaBila.transform.localPosition;
         arrow.gameObject.SetActive(false);
+
+        // Get a reference for our line renderer, or add one if not present.
+        lineRenderer = GetComponent<LineRenderer>();
+        if (lineRenderer == null) {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        // Set the line renderer properties
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 2;
+        lineRenderer.material = material;
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+        lineRenderer.startColor = startColour;
+        lineRenderer.endColor = endColour;
+        lineRenderer.numCapVertices = 20;
     }
 
     // Update is called once per frame
@@ -28,35 +53,47 @@ public class BallController : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0) == true)
             {
-                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition = Input.mousePosition;
                 arrow.gameObject.SetActive(true);
                 Debug.Log("Click started");
+                Vector3 startPos = bialaBila.transform.localPosition + Vector3.forward;
+                lineRenderer.SetPosition(0, startPos);
+                lineRenderer.SetPosition(1, startPos);
+                lineRenderer.enabled = true;
             }
 
             if (Input.GetMouseButton(0) == true)
             {
-                Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 position = Input.mousePosition;
 
-                Vector2 direction = mousePosition - position;
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                Vector2 rotation = new Vector2(0,angle);
-                Quaternion qua = Quaternion.Euler(rotation);
+                Vector2 def = mousePosition - position;
+                float rad = Mathf.Atan2(def.x, def.y);
+                float angle = rad*Mathf.Rad2Deg;
+                Vector3 rot = new Vector3(0, angle, 0);
+                Quaternion qua = Quaternion.Euler(rot);
 
-                arrow.rotation = qua;
-                arrow.position = bialaBila.transform.position;
+                arrow.localRotation = qua;
+                arrow.transform.position = bialaBila.transform.position;
+
+                Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward;
+                lineRenderer.SetPosition(1, endPos);
+
             }
 
             if (Input.GetMouseButtonUp(0) == true)
             {
-                Vector2 upPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 upPosition = Input.mousePosition;
 
-                Vector2 direction = mousePosition - upPosition;
-                Vector2 force = new Vector2(direction.x, direction.y) * power;
+                Vector2 def = mousePosition - upPosition;
+                Vector2 add = new Vector2(def.x, def.y);
 
-                bialaRigid.AddForce(force);
+                bialaRigid.AddForce(add*power);
                 arrow.gameObject.SetActive(false);
 
                 Debug.Log("Finish clicking");
+
+                // We've let go, so hide the drag line
+                lineRenderer.enabled = false;
             }
         }
     }
--- Assets/Script/Hole.cs
+++ Assets/Script/Hole.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject Bile_kolorowe;
+	public GameObject biala_bila;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector2 originalCueBallPosition;
+
+	void Start() {
+		originalCueBallPosition = biala_bila.transform.position;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (var transform in Bile_kolorowe.GetComponentsInChildren<Transform>()) {
+			if (transform.name == collision.gameObject.name) {
+				var objectName = collision.gameObject.name;
+				GameObject.Destroy(collision.gameObject);
+
+				var ballNumber = int.Parse(objectName.Replace("Ball", ""));
+			}
+		}
+
+		if (biala_bila.transform.name == collision.gameObject.name) {
+			biala_bila.transform.position = originalCueBallPosition;
+		}
+	}
 }
