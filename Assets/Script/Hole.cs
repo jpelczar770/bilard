9a9259d60740634c304a4eb3b4241a8122bc5ccc
Paula <paula.banachh@gmail.com>
2023-05-23 12:39:34 +0200

    działają łuzy!

--- Assets/Script/Hole.cs
+++ Assets/Script/Hole.cs
@@ -2,27 +2,21 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-	public GameObject Bile_kolorowe;
 	public GameObject biala_bila;
 
-	private Vector2 originalCueBallPosition;
+    public Vector3 position = new Vector3 (-11,0,-0.5f);
 
-	void Start() {
-		originalCueBallPosition = biala_bila.transform.position;
-	}
-
-	void OnCollisionEnter(Collision collision) {
-		foreach (var transform in Bile_kolorowe.GetComponentsInChildren<Transform>()) {
-			if (transform.name == collision.gameObject.name) {
-				var objectName = collision.gameObject.name;
-				GameObject.Destroy(collision.gameObject);
-
-				var ballNumber = int.Parse(objectName.Replace("Ball", ""));
-			}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "bila") {
+		    Debug.Log("Hit: " + other.transform.name);
+		    other.gameObject.SetActive(false);
+		    Debug.Log(other.gameObject);
 		}
 
-		if (biala_bila.transform.name == collision.gameObject.name) {
-			biala_bila.transform.position = originalCueBallPosition;
+		if (other.gameObject.name == "biala_bila") {
+		    Instantiate(biala_bila, position, Quaternion.identity);
+	        other.gameObject.SetActive(false);
 		}
+
 	}
 }
