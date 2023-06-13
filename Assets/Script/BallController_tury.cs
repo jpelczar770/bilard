116f131efdd7cb73a29edd35f0244904d71f1dcc
Igor <igor.jan.kolodziej@gmail.com>
2023-06-13 16:55:34 +0200

    szkielet tur

---
+++ Assets/Script/BallController_tury.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BallController_tury : MonoBehaviour
+{
+    public TMP_Text gracz_text;
+    public string player;
+    public Material material;
+    public int liczba_inst = 0;
+    public float startWidth = 0.2f;
+    public float endWidth = 0.0f;
+    public Color startColour = Color.white;
+    public Color endColour = Color.clear;
+    public Vector3 respawnposition = new Vector3(-11, 0, -0.5f);
+    public manager_luz_tury man;
+    public bool przed_ruchem;
+
+    /// <summary>
+    /// //////////////
+    /// </summary>
+    public int turka = 0;
+
+    private LineRenderer lineRenderer;
+
+    [SerializeField] GameObject bialaBila = null;
+    [SerializeField] float power = 30f;
+    [SerializeField] Transform arrow = null;
+    [SerializeField] List<Rigidbody2D>ballList = new List<Rigidbody2D>();
+
+    Vector2 mousePosition = new Vector2();
+    Vector2 zero = new Vector2 (0,0);
+    Rigidbody2D bialaRigid = null;
+    SpriteRenderer sprite = null;
+    CircleCollider2D ballCollider = null;
+    Vector2 bialaBilaDefaultPosition = new Vector2();
+
+    public void Tura(int tura)
+    {       if (tura == 0)
+            { 
+                    player = "player1";
+                    turka = 1; 
+             }
+            else if (tura == 1) {
+                 player = "player2";
+                 turka = 0; }
+             przed_ruchem = true;
+        gracz_text.text = player;
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tura(turka);
+        // Display the capture text with the player's name
+
+        bialaRigid = bialaBila.GetComponent<Rigidbody2D>();
+        sprite = bialaBila.GetComponent<SpriteRenderer>();
+        ballCollider = bialaBila.GetComponent<CircleCollider2D>();
+        bialaBilaDefaultPosition = bialaBila.transform.localPosition;
+        arrow.gameObject.SetActive(false);
+
+        // Get a reference for our line renderer, or add one if not present.
+        lineRenderer = GetComponent<LineRenderer>();
+        if (lineRenderer == null) {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+
+        // Set the line renderer properties
+        lineRenderer.enabled = false;
+        lineRenderer.positionCount = 2;
+        lineRenderer.material = material;
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+        lineRenderer.startColor = startColour;
+        lineRenderer.endColor = endColour;
+        lineRenderer.numCapVertices = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        bool bilewruchu = false;
+        foreach (Rigidbody2D bila in ballList)
+        {
+           if (bila.velocity.magnitude < 1.25)
+            {
+                bila.velocity = zero;
+            }
+            else
+            {
+                bilewruchu = bilewruchu | true;
+            }
+        }
+
+        if (bialaBila.activeSelf == true)
+        {
+
+            if (bialaRigid.velocity.magnitude < 1.25)
+            {
+                bialaRigid.velocity = zero;
+            }
+            else
+            {
+                bilewruchu = bilewruchu | true;
+            }
+
+                if (!bilewruchu)
+                {
+                if (przed_ruchem && man.wbitawruchu == false)
+                {
+                    if (turka == 0)
+                    { Tura(1); }
+                    else if (turka == 1)
+                    { Tura(0); }
+                }
+                if (Input.GetMouseButtonDown(0) == true)
+                    {
+                        mousePosition = Input.mousePosition;
+                        arrow.gameObject.SetActive(true);
+                        Vector3 startPos = bialaBila.transform.localPosition + Vector3.forward;
+                        lineRenderer.SetPosition(0, startPos);
+                        lineRenderer.SetPosition(1, startPos);
+                        lineRenderer.enabled = true;
+                    }
+
+                    if (Input.GetMouseButton(0) == true)
+                    {
+                        Vector2 position = Input.mousePosition;
+
+                        Vector2 def = mousePosition - position;
+                        float rad = Mathf.Atan2(def.x, def.y);
+                        float angle = rad*Mathf.Rad2Deg;
+                        Vector3 rot = new Vector3(0, angle, 0);
+                        Quaternion qua = Quaternion.Euler(rot);
+
+                        arrow.localRotation = qua;
+                        arrow.transform.position = bialaBila.transform.position;
+
+                        Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward;
+                        lineRenderer.SetPosition(1, endPos);
+
+                    }
+
+                    if (Input.GetMouseButtonUp(0) == true)
+                    { //puszczenie kijka
+                        przed_ruchem = false;
+                        liczba_inst += 1;
+                        man.wbitawruchu = false;
+                        
+                        Vector2 upPosition = Input.mousePosition;
+
+                        Vector2 def = mousePosition - upPosition;
+                        Vector2 add = new Vector2(def.x, def.y);
+
+                        bialaRigid.AddForce(add*power);
+                        arrow.gameObject.SetActive(false);
+
+                        lineRenderer.enabled = false;
+                    }
+                }
+
+        }
+
+        if (!sprite.enabled & !bilewruchu)
+        {
+            bialaBila.transform.position = respawnposition;
+            sprite.enabled = true;
+            ballCollider.enabled = true;
+        }
+
+    }
+    
+}
---
+++ Assets/manager_luz_tury.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class manager_luz_tury : MonoBehaviour
+{
+    public string player1Tag;
+    public string player2Tag;
+    int wbite = 0;
+    public Transform pozycja;
+    public BallController_tury ref_BC;
+    private string Kula_2;
+    public bool wbitawruchu;
+    /// <summary>
+    /// ////////
+    /// </summary>
+
+    // Start is called before the first frame updatezdzc
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void wbita(GameObject kula)
+    {
+        wbitawruchu = true;
+        if (wbite == 0)
+        {
+            if (kula.tag == "pe³ne")
+            { 
+                Kula_2 = "po³ówki"; 
+            }
+            if (ref_BC.liczba_inst % 2 != 0)
+            {
+                player1Tag = kula.tag;
+                player2Tag = Kula_2;
+            }
+            else if (ref_BC.liczba_inst % 2 == 0)
+            {
+                player2Tag = kula.tag;
+                player1Tag = Kula_2;
+            }
+        }
+
+        wbite += 1;
+        Vector3 p = pozycja.position;
+        p.x = p.x + wbite * 3.0f;
+        kula.transform.position = p;
+    }
+
+
+}
